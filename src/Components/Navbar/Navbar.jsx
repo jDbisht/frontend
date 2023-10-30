0979ff8fcd6818804b6bd5c6c213467c7f0d83c4
namrata shirsat <namrata.shirsaat@gmail.com>
2023-10-30 16:19:06 +0530

    changes navbar 1

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,31 +1,39 @@
 import React from "react";
-import './Navbar.css';
+import "./Navbar.css";
 
 import logo from "../Assets/logo.png";
 import cartLogo from "../Assets/cart_icon.png";
-
 
 export const Navbar = () => {
   return (
     <nav className="row">
       <div className="cols-4 brand">
-          <img src={logo} alt="shoppers" />
-          <p>SHOPPER</p>
+        <img src={logo} alt="shoppers" />
+        <p>SHOPPER</p>
       </div>
       <div className="cols-4">
+        <ul>
+          <li>
+            <a href="/"> Shop</a>
+          </li>
+          <li>
+            <a href="/"> Men</a>
+          </li>
+          <li>
+            <a href="/"> Womens</a>
+          </li>
+          <li>
+            <a href="/"> Kids</a>
+          </li>
+        </ul>
+      </div>
 
-          <ul>
-            <li>Shop</li>
-            <li>Men</li>
-            <li>Womens</li>
-            <li>Kids</li>
-          </ul>
-        </div>
-
-      <div className="cols-4">
-        <button type="submit">Login</button>
+      <div className="cols-4 btn-grp">
+        <button type="submit" id="login">
+          Login
+        </button>
         <img src={cartLogo} alt="" />
-        </div>
+      </div>
     </nav>
   );
 };
